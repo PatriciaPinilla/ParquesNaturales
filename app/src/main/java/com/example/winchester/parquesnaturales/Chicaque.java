22664746297223be49e019b05ae4d989e72fb6e8
PatriciaPinilla <claupinilla@uan.edu.co>
2016-06-01 22:51:11 -0500

    Intento maps 1

--- app/src/main/java/com/example/winchester/parquesnaturales/Chicaque.java
+++ app/src/main/java/com/example/winchester/parquesnaturales/Chicaque.java
@@ -1,28 +1,54 @@
 package com.example.winchester.parquesnaturales;
 
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import java.lang.*;
 
-public class Chicaque extends Lista {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chicaque);
+    public class Chicaque extends Lista {
+
+
+        protected boolean isRouteDisplayed() {
+            return false;
+        }
+
+                public void onLocationChanged(Location location) {
+        }
+
+
+        public void onProviderDisable(String provider) {
+        }
+
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+
+        public void onProviderEnable(String provider) {
+        }
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_chicaque);
+        }
+
+        public void B_SERVICIOS(View Next) {
+            Intent intent = new Intent(this, ServChicaque.class);
+            startActivity(intent);
+        }
+
+        public void B_ACTIVIDADES(View Next) {
+            Intent intent = new Intent(this, ServChicaque.class); // Crear Actividades Chicaque new activity
+            startActivity(intent);
+        }
+
+        public void B_UBICACION(View Next) {
+            Intent intent = new Intent(this, ServChicaque.class); // Crear ubicación en mapa pdte
+            startActivity(intent);
+        }
     }
-    public void B_SERVICIOS (View Next) {
-        Intent intent = new Intent(this, ServChicaque.class);
-        startActivity(intent);
-    }
-    public void B_ACTIVIDADES (View Next) {
-        Intent intent = new Intent(this, ServChicaque.class); // Crear Actividades Chicaque new activity
-        startActivity(intent);
-    }
-    public void B_UBICACION (View Next) {
-        Intent intent = new Intent(this, ServChicaque.class); // Crear ubicación en mapa pdte
-        startActivity(intent);
-    }
-}
